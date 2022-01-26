ab789bfcc134237a8cadb70fb77401228780d519
Daniel <kr.danielreis@gmail.com>
2022-01-26 10:45:33 -0300

    @DanielSantosReis [ADD função Get no Controller]

--- src/main/java/com/generation/diretoDaTerra/Controller/CategoriaController.java
+++ src/main/java/com/generation/diretoDaTerra/Controller/CategoriaController.java
@@ -1,16 +1,16 @@
 package com.generation.diretoDaTerra.Controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.generation.diretoDaTerra.Model.Categoria;
 import com.generation.diretoDaTerra.Repository.CategoriaRepository;
+import com.generation.diretoDaTerra.Util.Tipo;
 
 
 @RestController
@@ -22,8 +22,23 @@ public class CategoriaController {
 	private CategoriaRepository repository;
 	
 	@GetMapping
-	public ResponseEntity<List<Categoria>> GetAll(){
+	public ResponseEntity<List<Categoria>> getAll() {
 		return ResponseEntity.ok(repository.findAll());
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> getById (@PathVariable Long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	@GetMapping("/tipo/{tipo}")
+	public ResponseEntity<List<Categoria>> getByTipo(@PathVariable Tipo tipo) {
+		return ResponseEntity.ok(repository.findAllByTipoContainingIgnoreCase(tipo));
+	
+	}
+	@GetMapping("/descricao/{descricao}")
+	public ResponseEntity<List<Categoria>> getByDescricao(@PathVariable String descricao) {
+		return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(descricao));
+	
 	}
 	
 	
--- src/main/java/com/generation/diretoDaTerra/Repository/CategoriaRepository.java
+++ src/main/java/com/generation/diretoDaTerra/Repository/CategoriaRepository.java
@@ -1,13 +1,13 @@
 package com.generation.diretoDaTerra.Repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.generation.diretoDaTerra.Model.Categoria;
 import com.generation.diretoDaTerra.Util.Tipo;
 
 public interface CategoriaRepository  extends JpaRepository<Categoria, Long>{
 	public List<Categoria> findAllByTipoContainingIgnoreCase(Tipo tipo);
+
+	public List<Categoria> findAllByDescricaoContainingIgnoreCase(String descricao);
 	
 }
